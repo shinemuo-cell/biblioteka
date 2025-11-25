278ab3acb502ccfb0e19d6b9a8d1f543b9a5ac2c
Pylimas <kristina.db@hotmail.com>
2025-11-25 12:23:58 +0200

    garantijos ner

--- script.js
+++ script.js
@@ -14,14 +14,16 @@ db.connection(err=>{
         console.err('Nepavyko prisijungti prie DB:',err);
         return;
     }
-    console.log('Prosijungta');
+    console.log('Prisijungta');
 
     const createTableQuery= `
-    CREATE TABLE IF NOT EXISTS users(
+    CREATE TABLE IF NOT EXISTS books(
     id INT AUTO_INCREMENT PROMARY KEY,
     name VARCHAR(100),
-    surname VARCHAR(100),
-    email VARCHAR(100)
+    author VARCHAR(100),
+    metai INT,
+    isbn INT,
+    kiekis INT
     )`;
     
     db.query(createTableQuery, (err,result)=>{
@@ -32,31 +34,14 @@ db.connection(err=>{
     
 });
 
-// Vartotojo iterpimas
-app.get('/add', (req, res) => {
-  const insertQuery = `INSERT INTO users (name, email) VALUES ('Jonas', 'jonas@example.com')`;
-  db.query(insertQuery, (err, result) => {
+// Visu knygu isvedimas
+app.get('/books', (req, res) => {
+  db.query('SELECT * FROM books', (err, results) => {
     if (err) throw err;
-    res.send('Vartotojas pridėtas!');
-  });
-});
-
-// Visu vartotoju isvedimas
-app.get('/users', (req, res) => {
-  db.query('SELECT * FROM users', (err, results) => {
-    if (err) throw err;
-    res.render('users', { users: results });
+    res.render('books', { books: results });
   });
 });
 
 app.listen(3000, () => {
   console.log('Serveris paleistas http://localhost:3000');
 });
-
-//vartotoju pateikimas
-//<h1>Vartotojai</h1>
-//  <ul>
-//    <% users.forEach(user => { %>
-//      <li><%= user.name %> – <%= user.email %></li>
-//    <% }) %>
-//  </ul>
