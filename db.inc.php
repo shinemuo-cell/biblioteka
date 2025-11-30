f4332d5a681e39dd5e6fd78b11ed10310ea449a5
Pylimas <kristina.db@hotmail.com>
2025-11-30 16:49:26 +0200

    sujungimas

--- db.inc.php
+++ db.inc.php
@@ -1,5 +1,18 @@
 <?php
+#kodas skirtas prijungti db prie svetaines (naudojau xampp)
 
 $dsn = "mysql:host=localhost; dbname=library";
 #host nurodo kur pasiekti svetaine (standartinis, keistis neturetu)
-#dbname db pavadinimas, as pasivadinau library, pas jus gali buti kitaip (jei ka pasikeiskit)
+#dbname db pavadinimas, as pasivadinau library, pas jus gali buti kitaip (jei ka pasikeiskit i savo)
+
+$dbusername = "root";
+$dbpassword = "";
+#standartiniai dauomenys (pas visus default vienodai nebent pasikeitet arba naudojat mac)
+
+try{
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
+}catch(PDOExeption $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+
