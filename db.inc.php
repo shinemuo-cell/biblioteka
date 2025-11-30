1c9693eea5f6dce01ea89fdc2ad8ffe092f67189
Pylimas <kristina.db@hotmail.com>
2025-11-30 16:49:02 +0200

    kodas skirtas sujungti db su svetaine

---
+++ db.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$dsn = "mysql:host=localhost; dbname=library";
+#host nurodo kur pasiekti svetaine (standartinis, keistis neturetu)
+#dbname db pavadinimas, as pasivadinau library, pas jus gali buti kitaip (jei ka pasikeiskit)
