71c65bccc7c133e732d66672fd6d18055bab0fa2
Pylimas <kristina.db@hotmail.com>
2025-12-03 16:32:17 +0200

    failu sukurimai ir pakeitimai

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Pagrindinis</title>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+    </head>
+    <body>
+        <header>
+            <b><a class="logo">Bibliotekos valdymo sistema</a></b>
+            <nav>
+                <b><a class="pageLink">Prisijungti</a></b>
+            </nav>
+        </header>
+        <main>
+            <h1>Pagrindinis puslapis</h1>
+            <h3>Puslapio zemelapis</h3>
+            <h3>Kontaktai</h3>
+        </main>
+        <footer>
+            Kaunas, 2025. © Kristina DB, Viltė I., Vasarė M.
+        </footer>
+    </body>
+</html>
