6e80e5b2358096608b4c0229dac4b533b35cbfc5
Pylimas <kristina.db@hotmail.com>
2025-12-04 15:05:43 +0200

    vartotojo tipo tikrinimas

--- books.php
+++ books.php
@@ -1,6 +1,8 @@
 <?php
 include_once 'db.inc.php';
-
+if ($_SESSION['role'] !== 'admin'|| $_SESSION['role'] !== 'employee' || $_SESSION['role'] !== 'user') {
+    die("Neturite prieigos prie šio puslapio!");
+}
 ?>
 
 <!DOCTYPE html>
